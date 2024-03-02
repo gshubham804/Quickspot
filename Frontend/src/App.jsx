2ad216476333c952c915423b97fe091490326274
SHUBHAM GUPTA <gshubham804@gmail.com>
2024-03-02 10:04:26 +0530

    setup the redux toolkit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,27 +8,31 @@ import Signup from "./Components/pages/Signup";
 import Services from "./Components/pages/Services";
 import Booking from "./Components/pages/Booking";
 import ProductDataForm from "./Components/pages/ProductDataForm";
+import { Provider } from "react-redux";
+import store from './Components/redux/store';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="productdetails">
-            <Route index element={<ProductDetails />} />
-            <Route path="booking" element={<Booking />} />
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="productdetails">
+              <Route index element={<ProductDetails />} />
+              <Route path="booking" element={<Booking />} />
+            </Route>
+            <Route path="aboutus" element={<About />} />
+            <Route path="contactus" element={<Contact />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="services" element={<Services />} />
+            <Route path="productdataform" element={<ProductDataForm />} />
           </Route>
-          <Route path="aboutus" element={<About />} />
-          <Route path="contactus" element={<Contact />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="services" element={<Services />} />
-          <Route path="productdataform" element={<ProductDataForm />} />
-        </Route>
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </Router>
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ Frontend/src/Components/redux/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+  token: "",
+  user: null,
+  user_id: null,
+  email: "",
+  error: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logIn(state, action) {
+      state.isLoggedIn = action.payload.isLoggedIn;
+      state.token = action.payload.token;
+    },
+    signOut(state) {
+      state.isLoggedIn = false;
+      state.token = "";
+    },
+    updateRegisterEmail(state, action) {
+      state.email = action.payload.email;
+    },
+  },
+});
+
+export default authSlice.reducer;
